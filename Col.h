15c679a439082ad79cb3f231af89ff8f64e0303e
robertoBenavides <37338502+robertoBenavides@users.noreply.github.com>
2021-05-19 15:35:30 -0500

    Add files via upload

--- Col.h
+++ Col.h
@@ -7,16 +7,33 @@ struct Col
     string tipo;
     string tamanio;
     Col() {}
-    Col(string n, string t,string tam="0") {
+    Col(string n, string t, string tam = "0") {
         nombre = n;
         tipo = t;
-        tamanio=tam;
+        tamanio = tam;
     }
     string toString() {
         string del = "/";
-        if(tamanio=="0"){
-          return nombre + del + tipo;
+        if (tamanio == "0") {
+            return nombre + del + tipo;
         }
         return nombre + del + tipo + del + tamanio;
     }
 };
+
+
+/*struct Col
+{
+    string nombre;
+    string tipo;
+    Col() {}
+    Col(string n, string t) {
+        nombre = n;
+        tipo = t;
+    }
+    string toString() {
+        string del = "/";
+        return nombre + del + tipo;
+    }
+};*/
+
